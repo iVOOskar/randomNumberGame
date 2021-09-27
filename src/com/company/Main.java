5b45b4135e99d40f1c295af1c16cbb2465372f7c
oh210602 <OH210602@reigate.ac.uk>
2021-09-27 10:43:42 +0100

    randomNumberGame

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         Scanner input = new Scanner(System.in);
         Random random = new Random();
         int randomNum = (random.nextInt(100));
         //System.out.println(randomNum);
-
+        int i = 1;
         System.out.println("enter your first guess (1 to 100)");
         int guess1 = input.nextInt();
 
         while (guess1 != randomNum){
             System.out.println("that was wrong, enter your next number");
             int guess2 = input.nextInt();
+            i++;
             int diff1 = (randomNum - guess1);
             int diff2 = (randomNum - guess2);
             if (guess2 == randomNum || guess1 == randomNum){
@@ -31,12 +33,17 @@ public class Main {
                 }
                 if (diff2 < diff1) {
                     System.out.println("your getting warmer");
-                } else {
+                }
+                if (diff2 == diff1){
+                    System.out.println("still warm");
+                }
+                else {
                     System.out.println("your getting colder");
                 }
 
                 System.out.println("that was wrong, enter your next number");
                 guess1 = input.nextInt();
+                i++;
                 diff1 = (randomNum - guess1);
                 diff2 = (randomNum - guess2);
                 if (guess2 == randomNum || guess1 == randomNum){
@@ -51,13 +58,21 @@ public class Main {
                     }
                     if (diff1 < diff2) {
                         System.out.println("your getting warmer");
-                    } else {
+                    }
+                    if (diff2 == diff1){
+                        System.out.println("still warm");
+                    }
+                    else {
                         System.out.println("your getting colder");
                     }
                 }
             }
         }
         System.out.println("good job you guessed the number which was "+randomNum);
+        System.out.println("you guessed the number in "+i+" guesses");
+        long finnish = System.currentTimeMillis();
+        long timeElapsed = finnish - start;
+        System.out.println("you took "+timeElapsed/1000+" seconds");
 
 
 
