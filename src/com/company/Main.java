5745d9c5fe2e2a54b5e071b73fce8e2a7779776b
oh210602 <OH210602@reigate.ac.uk>
2021-09-27 10:10:23 +0100

    randomNumberGame

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int randomNum = (random.nextInt(100));
         System.out.println(randomNum);
 
-        System.out.println("enter your first guess (1-100)");
+        System.out.println("enter your first guess (1 to 100)");
         int guess1 = input.nextInt();
 
         while (guess1 != randomNum){
@@ -19,14 +19,43 @@ public class Main {
             int guess2 = input.nextInt();
             int diff1 = (randomNum - guess1);
             int diff2 = (randomNum - guess2);
-            if (diff1<0){
-                diff1 = diff1*-1;
+            if (guess2 == randomNum || guess1 == randomNum){
+                guess1 = guess2;
             }
-            if (diff2<0){
-                diff2 = diff2*-1;
-            }
-            if ()
+            else {
+                if (diff1 < 0) {
+                    diff1 = diff1 * -1;
+                }
+                if (diff2 < 0) {
+                    diff2 = diff2 * -1;
+                }
+                if (diff2 < diff1) {
+                    System.out.println("your getting warmer");
+                } else {
+                    System.out.println("your getting colder");
+                }
 
+                System.out.println("that was wrong, enter your next number");
+                guess1 = input.nextInt();
+                diff1 = (randomNum - guess1);
+                diff2 = (randomNum - guess2);
+                if (guess2 == randomNum || guess1 == randomNum){
+                    guess1 = guess2;
+             }
+                else {
+                    if (diff1 < 0) {
+                        diff1 = diff1 * -1;
+                    }
+                    if (diff2 < 0) {
+                        diff2 = diff2 * -1;
+                    }
+                    if (diff1 < diff2) {
+                        System.out.println("your getting warmer");
+                    } else {
+                        System.out.println("your getting colder");
+                    }
+                }
+            }
         }
         System.out.println("good job you guessed the number");
 
